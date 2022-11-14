ce4aa9dee8e30bf425ff8f3e10e3f0df972bd4c7
ignaciogudino <ignaciogudino1@hotmail.com>
2022-11-14 13:46:14 -0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  https: true,
-  port: 8080
+  devServer: {
+    https: true,
+  }
 })
