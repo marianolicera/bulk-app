7bf71db7f2182f964e999620ea4e7e89ea001a6a
ignaciogudino <ignaciogudino1@hotmail.com>
2022-11-14 13:36:30 -0300

    Vue HTTPS traffic

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  https: true,
+  port: 8080
 })
